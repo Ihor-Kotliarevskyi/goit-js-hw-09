d0c012c9e4025fc190680c11f489b3a7dedda9ee
Ihor Kotliarevskyi <jagor0707@gmail.com>
2025-10-01 22:44:35 +0300

    Task: 2-form - done

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -12,3 +12,59 @@ formContainer.innerHTML = `<form class="feedback-form" autocomplete="off">
   <button type="submit">Submit</button>
 </form>
 `;
+
+let formData = {
+  email: '',
+  message: '',
+};
+const FEEDBACK_KEY = 'feedback-form-state';
+
+const form = document.querySelector('.feedback-form');
+const input = form.querySelector('input');
+const textarea = form.querySelector('textarea');
+
+checkInput();
+checkTextarea();
+
+form.addEventListener('submit', handlerSubmit);
+input.addEventListener('input', handlerInput);
+textarea.addEventListener('input', handlerTextarea);
+
+function handlerSubmit(event) {
+  event.preventDefault();
+
+  if (formData.email && formData.message) {
+    console.log(formData.email, formData.message);
+  } else {
+    alert('Fill please all fields');
+    return;
+  }
+  event.currentTarget.reset();
+  localStorage.removeItem(FEEDBACK_KEY);
+  formData.email = '';
+  formData.message = '';
+}
+
+function handlerInput(event) {
+  formData.email = event.target.value;
+  localStorage.setItem(FEEDBACK_KEY, JSON.stringify(formData));
+}
+
+function handlerTextarea(event) {
+  formData.message = event.target.value;
+  localStorage.setItem(FEEDBACK_KEY, JSON.stringify(formData));
+}
+
+function checkInput() {
+  if (JSON.parse(localStorage.getItem(FEEDBACK_KEY)).email) {
+    input.value = JSON.parse(localStorage.getItem(FEEDBACK_KEY)).email;
+  }
+  return;
+}
+
+function checkTextarea() {
+  if (JSON.parse(localStorage.getItem(FEEDBACK_KEY)).message) {
+    textarea.value = JSON.parse(localStorage.getItem(FEEDBACK_KEY)).message;
+  }
+  return;
+}
