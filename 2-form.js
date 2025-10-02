cb3cfbb854d0c4bcfaab4659e48f8fbe30a08a13
Ihor-Kotliarevskyi <jagor0707@gmail.com>
2025-10-02 17:42:07 +0000

    Deploying to gh-pages from @ Ihor-Kotliarevskyi/goit-js-hw-09@66ca6d262c3a95abc2be7ed14b84af6e915b0ff7 🚀

--- 2-form.js
+++ 2-form.js
@@ -1,13 +1,2 @@
-import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */const l=document.querySelector(".form-container");l.innerHTML=`<form class="feedback-form" autocomplete="off">
-  <label>
-    Email
-    <input type="email" name="email" autofocus />
-  </label>
-  <label>
-    Message
-    <textarea name="message" rows="8"></textarea>
-  </label>
-  <button type="submit">Submit</button>
-</form>
-`;let e={email:"",message:""};const a="feedback-form-state",r=document.querySelector(".feedback-form"),o=r.querySelector("input"),m=r.querySelector("textarea");i();r.addEventListener("submit",s);r.addEventListener("input",n);function s(t){if(t.preventDefault(),e.email&&e.message)console.log(e.email,e.message);else{alert("Fill please all fields");return}t.currentTarget.reset(),localStorage.removeItem(a),e.email="",e.message=""}function n(t){t.target.nodeName==="INPUT"?e.email=t.target.value:e.message=t.target.value,localStorage.setItem(a,JSON.stringify(e))}function i(){JSON.parse(localStorage.getItem(a))&&(e.email=o.value=JSON.parse(localStorage.getItem(a)).email,e.message=m.value=JSON.parse(localStorage.getItem(a)).message)}
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */let e={email:"",message:""};const a="feedback-form-state",r=document.querySelector(".feedback-form"),l=r.querySelector("input"),s=r.querySelector("textarea");i();r.addEventListener("submit",o);r.addEventListener("input",m);function o(t){if(t.preventDefault(),e.email&&e.message)console.log(e.email,e.message);else{alert("Fill please all fields");return}t.currentTarget.reset(),localStorage.removeItem(a),e.email="",e.message=""}function m(t){t.target.nodeName==="INPUT"?e.email=t.target.value:e.message=t.target.value,localStorage.setItem(a,JSON.stringify(e))}function i(){JSON.parse(localStorage.getItem(a))&&(e.email=l.value=JSON.parse(localStorage.getItem(a)).email,e.message=s.value=JSON.parse(localStorage.getItem(a)).message)}
 //# sourceMappingURL=2-form.js.map
